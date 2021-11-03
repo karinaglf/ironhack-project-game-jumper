299a83c0ef7b988de6e56d8fea24a6114fd5751d
Karina Freitas <karinaglf@gmail.com>
2021-11-03 22:11:11 +0000

    add parallax background

---
+++ background.js
@@ -0,0 +1,43 @@
+class Background {
+    constructor() {
+        this.image = new Image();
+        this.image.src = "./images/background.png";
+        this.width = canvas.width;
+        this.height = canvas.height;
+        this.x = 0;
+        this.y = 0;
+        this.x2 = this.width - 1;
+        this.speed = 2;
+    }
+    moveBackground = () => {
+        this.x -= this.speed;
+        this.x2 -= this.speed;
+        if(this.x <= -this.width){
+            this.x = this.width + this.x2 - this.speed;
+        }
+        if(this.x2 <= -this.width){
+            this.x2 = this.width + this.x - this.speed;
+        }
+    }    
+    drawBackground = () => {
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height)
+    }
+}
+class Clouds extends Background {
+    constructor() {
+        super();
+        this.width = 931;
+        this.height = 111;
+        this.y = 80;
+        this.image.src = "/images/background-clouds.png";
+        this.speed = 1;
+    }
+}
+class Floor extends Background {
+    constructor() {
+        super();
+        this.image.src = "./images/background-floor.png";
+        this.speed = 2;
+    }
+}
--- game.js
+++ game.js
@@ -1,19 +1,19 @@
 class Game {
   constructor() {
-    this.bg = new Image();
-    this.bg.src = "./images/background.png";
-    this.bgCloud = new Image();
-    this.bgCloud.src = "./images/background-clouds.png";
-    this.bgCloudWidth = 931;
-    this.bgCloudX = 50; 
-    this.bgCloudX2 = 50 + this.bgCloudWidth;
+    //this.bg = new Image();
+    // this.bg.src = "./images/background.png";
+    // this.bgCloud = new Image();
+    // this.bgCloud.src = "./images/background-clouds.png";
+    // this.bgCloudWidth = 931;
+    // this.bgCloudX = 50; 
+    // this.bgCloudX2 = 50 + this.bgCloudWidth;
+    this.backgroundLayers = [new Background(), new Clouds(), new Floor()];
     this.character = new Character();
-    this.obstaclesArray = [new Obstacle(Math.random() * canvas.height)];
+    this.obstaclesArray = [new Obstacle()];
     this.obstaclesInterval = 100; //milliseconds for obstacles to appear
     this.obstaclesTimer = 0; // counter interval
-    this.obstacleType = ["bird", "cactus"]
+    //this.obstacleType = ["bird", "cactus"]
     this.lastTime = 1;
-    this.gameSpeed = 1;
     this.score = 0;
     this.scoreText = new Text("Score: " + this.score, 25, 50, "left", "black", "25");
     this.isGameover = false;
@@ -23,25 +23,6 @@ class Game {
   getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
   };
-
-  animateBackground = () => {
-    ctx.drawImage(this.bg, 0, 0, canvas.width, canvas.height);
-    
-    ctx.drawImage(this.bgCloud, this.bgCloudX, 70, this.bgCloudWidth , 111);
-    ctx.drawImage(this.bgCloud, this.bgCloudX2, 70, this.bgCloudWidth , 111);
-    
-    if(this.bgCloudX < -this.bgCloudWidth - this.gameSpeed) {
-      this.bgCloudX = 0;
-    } else {
-      this.bgCloudX -= this.gameSpeed;
-    }
-    if(this.bgCloudX2 < -this.bgCloudWidth  - this.gameSpeed) {
-      this.bgCloudX2 = 0;
-    } else {
-      this.bgCloudX2 -= this.gameSpeed;
-    }
-
-  }
 
   gameover = () => {
     //stop the game
@@ -56,18 +37,18 @@ class Game {
     this.scoreText.t = "Score: " + this.score;
   }
 
-  // spawnObstacles = (type) => {
-  //   let randomObstacle = this.obstacleType[(Math.floor(Math.random)*this.obstacleType.length)]
-  //   this.obstaclesArray.push(new Cactus());
-  //   this.obstaclesArray.push(new Bird());
-  //   this.obstaclesArray.sort((a,b) => a.y - b.y)
+  // addBackgroundLayers = () => {
+  //   let clouds = new Background("./images/background-clouds.png", 4)
+  //   let background = new Background("./images/background.png", 4)
+  //   this.backgroundLayers = [newBackground, newClouds]
+  //   this.backgroundLayers.push(new Clouds());
+  //   this.backgroundLayers.push(new BaseLayer());
   // }
 
-    spawnObstacles = () => {
-    let randomObstacle = this.obstacleType[(Math.floor(Math.random)*this.obstacleType.length)]
+  spawnObstacles = () => {
+    //let randomObstacle = this.obstacleType[(Math.floor(Math.random)*this.obstacleType.length)]
     this.obstaclesArray.push(new Cactus());
     this.obstaclesArray.push(new Bird());
-    this.obstaclesArray.sort((a,b) => a.y - b.y)
   }
 
   gameLoop = (timeStamp) => {
@@ -81,6 +62,10 @@ class Game {
 
     this.obstaclesArray.forEach((eachObstacle) => {
       eachObstacle.moveObstacle();
+    })
+
+    this.backgroundLayers.forEach((eachLayer) => {
+      eachLayer.moveBackground();
     })
 
     // Spawn obstacles at a certain interval
@@ -97,24 +82,20 @@ class Game {
     this.obstaclesArray.forEach((eachObstacle) => {
 
     if(this.character.checkCollision(eachObstacle)){
-        //console.log(eachObstacle);
         if (eachObstacle instanceof Cactus) {
-          this.gameover();
+          //this.gameover();
         } 
         if (eachObstacle instanceof Bird) {
           this.score +=1;
-        
-        // else {
-        //   this.score +=1;
-          // this.updateScore();
         }
-        //console.log("collision happened")
     }
     });
 
     //* 3. DRAW ELEMENTS
-    //ctx.drawImage(this.bg, 0, 0, canvas.width, canvas.height);
-    this.animateBackground();
+    this.backgroundLayers.forEach((eachLayer) => {
+      eachLayer.drawBackground();
+      console.log(this.backgroundLayers)
+    })
     this.scoreText.drawText();
     this.character.drawChar();
     this.obstaclesArray.forEach((eachObstacle) => {
