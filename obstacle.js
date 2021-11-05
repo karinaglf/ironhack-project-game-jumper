61e081d94eadd24f7d048bbc8be1b07b70396908
Karina Freitas <karinaglf@gmail.com>
2021-11-05 01:30:30 +0000

    better cactus image

--- obstacle.js
+++ obstacle.js
@@ -30,16 +30,14 @@ class Obstacle {
     }    
 }
 class Cactus extends Obstacle {
-    constructor () {
+    constructor (srcImage) {
         super()
-        this.spriteWidth = 261;
-        this.spriteHeight = 209;
-        this.width = this.spriteWidth / 3;
-        this.height = this.spriteHeight / 3;
-        this.image.src = "./images/cactus-sprite.png";
-        this.velocityX = 8 ;
+        this.width = 230 * 0.4;
+        this.height = 210 * 0.4;
+        this.image.src = "./images/cactus-1.png";
+        this.velocityX = 8;
         this.x = canvas.width;
-        this.y = 380;
+        this.y = 360;
     }
 }  
 class Bird extends Obstacle {
@@ -48,7 +46,7 @@ class Bird extends Obstacle {
         this.image.src = "./images/black-bird.png";
         this.x = canvas.width;
         this.y = Math.random() * canvas.height * 0.2;
-        this.velocityX = Math.random() * 0.1 + 3;
+        this.velocityX = Math.random() * 0.1 + 5;
         this.angle = 0;
         this.radius = 4;
     }   
@@ -60,10 +58,10 @@ class Bird extends Obstacle {
         this.height = this.spriteHeight / 2.5;
         this.x -= this.velocityX;
         this.y += Math.sin(this.angle) * this.radius;
-        this.angle += 0.2;
+        this.angle += 0.1;
         //remove obstacles from the array
         if (this.x < 0 - this.width) {
             this.markedForDeletion = true;
         }
-    }    
+    }
 }
