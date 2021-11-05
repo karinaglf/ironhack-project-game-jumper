b5a414657627037798187d9a49fa0ab42809db4f
Karina Freitas <karinaglf@gmail.com>
2021-11-05 12:27:40 +0000

    add level feature

--- game.js
+++ game.js
@@ -4,17 +4,23 @@ class Game {
     this.character = new Character();
     this.explosionArray = [new Explosion()];
     this.obstaclesArray = [new Obstacle()];
-    this.obstaclesInterval = 100; //milliseconds for obstacles to appear
-    this.obstaclesTimer = 0; // counter interval
-    //this.obstacleType = ["bird", "cactus"]
+    this.cactusTypes = [];
+    this.cactusInterval = 120;
+    this.birdsInterval = 50;
+    this.birdsTimer = 0;
+    this.cactusTimer = 0;
+    this.cactusVelocity = 7;
+    this.birdsVelocity = 8;
     this.lastTime = 1;
     this.score = 0;
-    this.scoreText = new Text("Score: " + this.score, 1000, 50, "right", "black");
+    this.scoreText = new Text("Score: " + this.score, 1000, 50,"right", "black");
+    this.levelText = new Text("Level: " + "1", 1000, 80,"right", "#3a3a3a");
     this.isGameover = false;
-    this.gameSpeed = 1;
+    this.levelControl = 1;
+    this.level = 1;
   }
 
-  //Random Function to be used as a utility 
+  //Random Function to be used as a utility
   getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
   };
@@ -26,23 +32,39 @@ class Game {
     canvas.style.display = "none";
     //show restart state
     gameoverScreen.style.display = "flex";
-  }
+  };
   increaseGameLevel = () => {
-    if(this.score % 10 === 0) {
-      this.obstaclesArray.forEach(eachObstacle => {
-        eachObstacle.velocityX += 0.1;
-      })
+    if (this.score > 1 && this.score % 10 == 0) {
+      this.cactusVelocity += 2;
+      this.birdsVelocity  += 3;
+      this.levelControl += 0.01;
+      this.level = Math.floor(this.levelControl);
     }
-  }
+  };
   updateScore = () => {
     this.scoreText.t = "Score: " + this.score;
-  }
-  spawnObstacles = () => {
-    //let randomObstacle = this.obstacleType[(Math.floor(Math.random)*this.obstacleType.length)]
-    this.obstaclesArray.push(new Cactus());
-    this.obstaclesArray.push(new Bird());
-  }
+    this.levelText.t = "Level: " + this.level;
+  };
 
+  cleanObstacles = () => {
+    this.obstaclesArray = this.obstaclesArray.filter(
+      (eachObstacle) => !eachObstacle.markedForDeletion
+    );
+  };
+  spawnObstacles = () => {
+    this.cactusTimer ++;
+    if (this.cactusTimer > this.cactusInterval) {
+      this.obstaclesArray.push(new Cactus(this.cactusVelocity));
+      this.cleanObstacles();
+      this.cactusTimer = 0;
+    }
+    this.birdsTimer ++;
+    if (this.birdsTimer > this.birdsInterval) {
+      this.obstaclesArray.push(new Bird(this.birdsVelocity));
+      this.cleanObstacles();
+      this.birdsTimer = 0;
+    }
+  };
   gameLoop = (timeStamp) => {
     //* 1. CLEAR THE CANVAS
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -53,58 +75,50 @@ class Game {
     this.character.animateChar();
     this.obstaclesArray.forEach((eachObstacle) => {
       eachObstacle.moveObstacle();
-    })
+    });
 
     this.backgroundLayers.forEach((eachLayer) => {
       eachLayer.moveBackground();
-    })
+    });
     this.explosionArray.forEach((eachExplosion) => {
       eachExplosion.animateExplosion();
-    })
+    });
 
-    // Spawn obstacles at a certain interval
-    if(this.obstaclesTimer > this.obstaclesInterval) {
-      this.spawnObstacles();
-      //clean array when it is outside canvas
-      this.obstaclesArray = this.obstaclesArray.filter(eachObstacle => !eachObstacle.markedForDeletion)
-      this.obstaclesTimer = 0;
-      //console.log(this.obstaclesArray);
-    } else {
-      this.obstaclesTimer += 1;
-    }
-
+    this.spawnObstacles();
     this.obstaclesArray.forEach((eachObstacle, index, obstaclesArray) => {
-
-    if(this.character.checkCollision(eachObstacle, index)){
+      if (this.character.checkCollision(eachObstacle, index)) {
         if (eachObstacle instanceof Cactus) {
           this.gameover();
-        } 
+        }
         if (eachObstacle instanceof Bird) {
-          this.score +=1;
+          this.score += 1;
           //console.log(this.score);
           //console.log(index)
           obstaclesArray.splice(index, 1);
-          this.explosionArray.push(new Explosion(eachObstacle.x, eachObstacle.y))
+          this.explosionArray.push(
+            new Explosion(eachObstacle.x, eachObstacle.y)
+          );
         }
-    }
+      }
     });
+    this.increaseGameLevel();
 
     //* 3. DRAW ELEMENTS
     this.backgroundLayers.forEach((eachLayer) => {
       eachLayer.drawBackground();
-    })
+    });
     this.scoreText.drawText();
+    this.levelText.drawText();
     this.character.drawChar();
     this.obstaclesArray.forEach((eachObstacle) => {
       eachObstacle.drawObstacle();
-    })
+    });
     this.explosionArray.forEach((eachExplosion) => {
       eachExplosion.drawExplosion();
-    })          
+    });
     //* 4. ANIMATION FRAME AND LOGIC
     this.updateScore();
-    this.increaseGameLevel();
-    if(!this.isGameover) {
+    if (!this.isGameover) {
       requestAnimationFrame(this.gameLoop);
     }
 
--- obstacle.js
+++ obstacle.js
@@ -1,66 +1,75 @@
 class Obstacle {
-    constructor() {
-        this.image = new Image();
-        this.spriteWidth = 261;
-        this.spriteHeight = 209;
-        this.frameX = 0;
-        this.width = this.spriteWidth * 2;
-        this.height = this.spriteHeight * 2;
-        this.x = canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.width = 80;
-        this.height = 80;
-        this.velocityX = 8;
-        this.markedForDeletion = false;
+  constructor(velocityX) {
+    this.image = new Image();
+    this.spriteWidth = 261;
+    this.spriteHeight = 209;
+    this.frameX = 0;
+    this.width = this.spriteWidth * 2;
+    this.height = this.spriteHeight * 2;
+    this.x = canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.width = 80;
+    this.height = 80;
+    this.velocityX = velocityX;
+    this.markedForDeletion = false;
+  }
+
+  drawObstacle = () => {
+    //ctx.fillStyle = "black";
+    //ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.frameX * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  };
+
+  moveObstacle = () => {
+    this.x -= this.velocityX;
+    //remove obstacles from the array
+    if (this.x < 0 - this.width) {
+      this.markedForDeletion = true;
     }
-
-    drawObstacle = () => {
-        //ctx.fillStyle = "black";
-        //ctx.fillRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, this.frameX  * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
-    }
-
-    moveObstacle = () => {
-        this.x -= this.velocityX;
-        //remove obstacles from the array
-        if (this.x < 0 - this.width) {
-            this.markedForDeletion = true;
-        }
-
-    }    
+  };
 }
 class Cactus extends Obstacle {
-    constructor (srcImage) {
-        super()
-        this.width = 230 * 0.4;
-        this.height = 210 * 0.4;
-        this.image.src = "./images/cactus-1.png";
-        this.velocityX = 7;
-        this.x = canvas.width;
-        this.y = 360;
-    }
-}  
+  constructor(velocityX) {
+    super(velocityX);
+    this.width = 230 * 0.4;
+    this.height = 210 * 0.4;
+    this.image.src = "./images/cactus-1.png";
+    this.velocityX = 7;
+    this.x = canvas.width;
+    this.y = 360;
+  }
+}
 class Bird extends Obstacle {
-    constructor () {
-        super()
-        this.image.src = "./images/black-bird.png";
-        this.x = canvas.width;
-        this.y = Math.random() * canvas.height * 0.2;
-        this.velocityX = 8;
-        this.angle = 0;
-        this.radius = 4;
-    }   
-    moveObstacle = () => {
-        this.spriteWidth = 261;
-        this.spriteHeight = 291;
-        this.width = this.spriteWidth / 2.5;
-        this.height = this.spriteHeight / 2.5;
-        this.x -= this.velocityX;
-        this.y += Math.sin(this.angle) * this.radius;
-        this.angle += 0.1;
-        //remove obstacles from the array
-        if (this.x < 0 - this.width) {
-            this.markedForDeletion = true;
-        }
+  constructor(velocityX) {
+    super(velocityX);
+    this.image.src = "./images/black-bird.png";
+    this.x = canvas.width;
+    this.y = Math.random() * canvas.height * 0.2;
+    this.velocityX = 8;
+    this.angle = 0;
+    this.radius = 4;
+  }
+  moveObstacle = () => {
+    this.spriteWidth = 261;
+    this.spriteHeight = 291;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.x -= this.velocityX;
+    this.y += Math.sin(this.angle) * this.radius;
+    this.angle += 0.1;
+    //remove obstacles from the array
+    if (this.x < 0 - this.width) {
+      this.markedForDeletion = true;
     }
+  };
 }
